eadc18251cb72f3a060ff4a0bd8ba96e0dee2d3f
Gyuwon <gyuwon@live.com>
2019-02-23 17:35:21 +0900

    Add interface IEventHandler

---
+++ source/Loom.EventSourcing/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Loom.EventSourcing
+{
+    public interface IEventHandler<T>
+    {
+        T Handle(T state, object eventPayload);
+    }
+
+    // TODO: Implement T HandleRange(T state, IEnumerable<object> eventPayloads) method.
+}
