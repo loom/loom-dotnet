94d249d9e00740972dfa117e760d9f6ef9b82c42
Gyuwon <gyuwon@live.com>
2019-02-23 17:50:06 +0900

    Add interface ISnapshotter

---
+++ source/Loom.EventSourcing/ISnapshotter.cs
@@ -0,0 +1,10 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface ISnapshotter
+    {
+        Task TakeSnapshot(Guid sourceId);
+    }
+}
