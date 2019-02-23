ccdc37880d24a0867cd48dc77de808a56d2a9359
Gyuwon <gyuwon@live.com>
2019-02-23 17:48:01 +0900

    Add interface ISnapshotReader

---
+++ source/Loom.EventSourcing/ISnapshotReader.cs
@@ -0,0 +1,11 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface ISnapshotReader<T>
+    {
+        // TODO: Change return type to Task<T?>.
+        Task<T> TryRestoreSnapshot(Guid sourceId);
+    }
+}
