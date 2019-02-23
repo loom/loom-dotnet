2e9a2df5ca8e9f975075d2ff4d9a03e51b957745
Gyuwon <gyuwon@live.com>
2019-02-23 17:24:50 +0900

    Add interface ICommand

---
+++ source/Loom.EventSourcing.Contracts/ICommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Loom.EventSourcing
+{
+    using Loom.Messaging;
+    using System;
+
+    public interface ICommand : IPartitioned
+    {
+        Guid TargetId { get; }
+        dynamic Payload { get; }
+
+        // TODO: Implement IPartitioned.PartitionKey property.
+    }
+}
