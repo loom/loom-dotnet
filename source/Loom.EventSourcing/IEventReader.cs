838a07c52447b65c1e04fe00b7960179c7b7b3af
Gyuwon <gyuwon@live.com>
2019-02-23 17:43:38 +0900

    Add interface IEventReader

---
+++ source/Loom.EventSourcing/IEventReader.cs
@@ -0,0 +1,14 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IEventReader
+    {
+        Task<IEnumerable<object>> QueryPayloads(
+            Guid sourceId, int afterVersion);
+    }
+
+    // TODO: Implement Task<IEnumerable<object>> QueryPayloads(Guid sourceId) method.
+}
