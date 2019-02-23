48ef4a0f3a146fd2781763c3547b612262dfcd9b
Gyuwon <gyuwon@live.com>
2019-02-23 17:21:49 +0900

    Add interface IEvent

---
+++ source/Loom.EventSourcing.Contracts/IEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Loom.EventSourcing
+{
+    using Loom.Messaging;
+    using System;
+
+    public interface IEvent : IVersioned, IPartitioned
+    {
+        Guid SourceId { get; }
+        DateTime RaisedAt { get; }
+        dynamic Payload { get; }
+
+        // TODO: Implement IPartitioned.PartitionKey property.
+    }
+}
