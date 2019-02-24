f82efad13732d7067f85aa97c670d18b45feb7a8
Gyuwon <gyuwon@live.com>
2019-02-24 13:26:59 +0900

    Add interface IMessage

---
+++ source/Loom.Messaging.Contracts/IMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace Loom.Messaging
+{
+    public interface IMessage
+    {
+        string Id { get; }
+        string OperationId { get; }
+        // TODO: Chanage property type to string?.
+        string Contributor { get; }
+        // TODO: Chanage property type to string?.
+        string ParentId { get; }
+        dynamic Data { get; }
+    }
+}
