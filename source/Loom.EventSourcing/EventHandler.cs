c5442b30c01fba89df9e6b0b7e30962280162791
Gyuwon <gyuwon@live.com>
2019-02-26 18:29:20 +0900

    Add class EventHandler<T>
    EventHandler<T> class provides default implementation of IEventHandler<T>
    interface.

---
+++ source/Loom.EventSourcing/EventHandler.cs
@@ -0,0 +1,59 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Reflection;
+
+    public abstract class EventHandler<T> : IEventHandler<T>
+    {
+        private readonly ImmutableDictionary<Type, MethodInfo> _handlers;
+
+        protected EventHandler()
+        {
+            const BindingFlags bindingFlags
+                = BindingFlags.Instance
+                | BindingFlags.Public
+                | BindingFlags.NonPublic;
+
+            IEnumerable<(Type eventPayloadType, MethodInfo handler)> query =
+                from method in GetType().GetMethods(bindingFlags)
+
+                where method.Name == "Handle"
+                where method.ReturnType == typeof(T)
+
+                let parameters = method.GetParameters()
+                where parameters.Length == 2
+
+                let stateParam = parameters[0]
+                where stateParam.ParameterType == typeof(T)
+
+                let eventPayloadParam = parameters[1]
+                let eventPayloadType = eventPayloadParam.ParameterType
+                where eventPayloadType != typeof(object)
+
+                select (eventPayloadType, handler: method);
+
+            _handlers = query.ToImmutableDictionary(
+                keySelector: t => t.eventPayloadType,
+                elementSelector: t => t.handler);
+        }
+
+        T IEventHandler<T>.Handle(T state, object eventPayload)
+        {
+            Type eventPayloadType = eventPayload.GetType();
+            _handlers.TryGetValue(eventPayloadType, out MethodInfo handler);
+            switch (handler)
+            {
+                case MethodInfo _:
+                    object[] arguments = new[] { state, eventPayload };
+                    return (T)handler.Invoke(this, arguments);
+
+                default:
+                    string message = $"Cannot handle the event of type {eventPayloadType}";
+                    throw new InvalidOperationException(message);
+            }
+        }
+    }
+}
---
+++ source/Loom.Tests/EventSourcing/EventHandler_specs.cs
@@ -0,0 +1,73 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+    using AutoFixture;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EventHandler_specs
+    {
+        public class State
+        {
+            public State(int value) => Value = value;
+
+            public int Value { get; }
+        }
+
+        public class EventHandler : EventHandler<State>
+        {
+            private State Handle(State state, ValueAdded eventPayload)
+                => new State(value: state.Value + eventPayload.Value);
+        }
+
+        public class UnknownEventPayload
+        {
+        }
+
+        public class ValueAdded
+        {
+            public ValueAdded(int value) => Value = value;
+
+            public int Value { get; }
+        }
+
+        [TestMethod]
+        public void sut_implements_IEventHandlerT()
+        {
+            Type sut = typeof(EventHandler<State>);
+            sut.Should().Implement<IEventHandler<State>>();
+        }
+
+        [TestMethod]
+        public void given_unknown_event_payload_type_then_Handle_throws_exception()
+        {
+            // Arrange
+            IEventHandler<State> sut = new EventHandler();
+            State state = new Fixture().Create<State>();
+            object eventPayload = new UnknownEventPayload();
+
+            // Act
+            Action action = () => sut.Handle(state, eventPayload);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public void given_known_event_payload_then_Handle_invokes_handler_correctly()
+        {
+            // Arrange
+            IEventHandler<State> sut = new EventHandler();
+            var builder = new Fixture();
+            State state = builder.Create<State>();
+            ValueAdded eventPayload = builder.Create<ValueAdded>();
+
+            // Act
+            State actual = sut.Handle(state, eventPayload);
+
+            // Assert
+            actual.Value.Should().Be(state.Value + eventPayload.Value);
+        }
+    }
+}
