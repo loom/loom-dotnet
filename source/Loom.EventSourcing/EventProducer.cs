8792e9964e5e9cd7cfd56b2b39813776626c115d
Gyuwon <gyuwon@live.com>
2019-03-02 14:24:43 +0900

    Add class EventProducer<T>
    EventProducer<T> class provides the default implementation of
    IEventProducer<T> interface.

---
+++ source/Loom.EventSourcing/EventProducer.cs
@@ -0,0 +1,65 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Reflection;
+
+    public class EventProducer<T> : IEventProducer<T>
+    {
+        private readonly ImmutableDictionary<Type, MethodInfo> _producers;
+
+        protected EventProducer()
+        {
+            const BindingFlags bindingFlags
+                = BindingFlags.Instance
+                | BindingFlags.Public
+                | BindingFlags.NonPublic;
+
+            IEnumerable<(Type commandPayloadType, MethodInfo producer)> query =
+                from method in GetType().GetMethods(bindingFlags)
+
+                where method.Name == "ProduceEventPayloads"
+                where method.ReturnType == typeof(IEnumerable<object>)
+
+                let parameters = method.GetParameters()
+                where parameters.Length == 2
+
+                let stateParam = parameters[0]
+                where stateParam.ParameterType == typeof(T)
+
+                let commandPayloadParam = parameters[1]
+                let commandPayloadType = commandPayloadParam.ParameterType
+                where commandPayloadType != typeof(object)
+
+                select (commandPayloadType, producer: method);
+
+            _producers = query.ToImmutableDictionary(
+                keySelector: t => t.commandPayloadType,
+                elementSelector: t => t.producer);
+        }
+
+        IEnumerable<object> IEventProducer<T>.ProduceEventPayloads(
+            T state, object commandPayload)
+        {
+            Type commandPayloadType = commandPayload.GetType();
+            switch (GetProducer(commandPayloadType))
+            {
+                case MethodInfo producer:
+                    object[] arguments = new[] { state, commandPayload };
+                    return (IEnumerable<object>)producer.Invoke(this, arguments);
+
+                default:
+                    string message = $"Cannot process the command of type {commandPayloadType}";
+                    throw new InvalidOperationException(message);
+            }
+        }
+
+        private MethodInfo GetProducer(Type commandPayloadType)
+        {
+            _producers.TryGetValue(commandPayloadType, out MethodInfo producer);
+            return producer;
+        }
+    }
+}
---
+++ source/Loom.Tests/EventSourcing/EventProducer_specs.cs
@@ -0,0 +1,85 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+    using System.Collections.Generic;
+    using AutoFixture;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EventProducer_specs
+    {
+        public class State
+        {
+            public State(int value) => Value = value;
+
+            public int Value { get; }
+        }
+
+        public class UnknownCommandPayload
+        {
+        }
+
+        public class DecreaseValueTwice
+        {
+        }
+
+        public class ValueChanged
+        {
+            public ValueChanged(int value) => Value = value;
+
+            public int Value { get; }
+        }
+
+        public class EventProducer : EventProducer<State>
+        {
+            private IEnumerable<object> ProduceEventPayloads(
+                State state, DecreaseValueTwice commandPayload)
+            {
+                int seed = state.Value;
+                yield return new ValueChanged(seed - 1);
+                yield return new ValueChanged(seed - 2);
+            }
+        }
+
+        [TestMethod]
+        public void sut_implements_IEventProducerT()
+        {
+            Type sut = typeof(EventProducer<State>);
+            sut.Should().Implement<IEventProducer<State>>();
+        }
+
+        [TestMethod]
+        public void given_unknown_command_payload_type_then_ProduceEventPayloads_throws_exception()
+        {
+            // Arrange
+            IEventProducer<State> sut = new EventProducer();
+            State state = new Fixture().Create<State>();
+            object commandPayload = new UnknownCommandPayload();
+
+            // Act
+            Action action = () => sut.ProduceEventPayloads(state, commandPayload);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public void given_known_command_payload_then_ProduceEventPayloads_returns_event_payloads_correctly()
+        {
+            // Arrange
+            IEventProducer<State> sut = new EventProducer();
+            State state = new Fixture().Create<State>();
+            object commandPayload = new DecreaseValueTwice();
+
+            // Act
+            IEnumerable<object> actual =
+                sut.ProduceEventPayloads(state, commandPayload);
+
+            // Assert
+            actual.Should().BeEquivalentTo(
+                new ValueChanged(state.Value - 1),
+                new ValueChanged(state.Value - 2));
+        }
+    }
+}
