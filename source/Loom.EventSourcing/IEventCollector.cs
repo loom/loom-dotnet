d9b981fd6d7d790ee3fcbe2c906db8cc868065b9
Gyuwon <gyuwon@live.com>
2019-03-22 22:18:01 +0900

    Redesign interface IEventCollector
    - Rename method 'Collect' to 'CollectEvents'
    - Remove unused parameters from method Collect

--- source/Loom.EventSourcing/IEventCollector.cs
+++ source/Loom.EventSourcing/IEventCollector.cs
@@ -6,14 +6,7 @@
 
     public interface IEventCollector
     {
-        // TODO: Change type of parameter 'contributor' to string?.
-        // TODO: Change type of parameter 'parentId' to string?.
-        Task Collect(
-            Guid streamId,
-            long firstVersion,
-            IEnumerable<object> events,
-            string operationId,
-            string contributor,
-            string parentId);
+        Task CollectEvents(
+            Guid streamId, long firstVersion, IEnumerable<object> events);
     }
 }
