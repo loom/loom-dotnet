180410eec79b80e81ef2ec9cb8099a232d9cf7f4
Gyuwon <gyuwon@live.com>
2019-03-23 19:45:54 +0900

    Add class FullNameTypeNameResolvingStrategy
    FullNameTypeResolvingStrategy class provides an implementation of
    ITypeNameResolvingStrategy interface. ResolveTypeName method returns the
    full name of the specified type.

---
+++ source/Loom.Messaging/FullNameTypeNameResolvingStrategy.cs
@@ -0,0 +1,9 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+
+    public class FullNameTypeNameResolvingStrategy : ITypeNameResolvingStrategy
+    {
+        public string ResolveTypeName(Type type) => type.FullName;
+    }
+}
---
+++ source/Loom.Tests/Messaging/FullNameTypeNameResolvingStrategy_specs.cs
@@ -0,0 +1,28 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FullNameTypeNameResolvingStrategy_specs
+    {
+        [TestMethod]
+        public void sut_implements_ITypeNameResolvingStrategy()
+        {
+            Type sut = typeof(FullNameTypeNameResolvingStrategy);
+            sut.Should().Implement<ITypeNameResolvingStrategy>();
+        }
+
+        [TestMethod]
+        public void ResolveTypeName_returns_full_name_of_type()
+        {
+            Type type = typeof(ReferencedType);
+            var sut = new FullNameTypeNameResolvingStrategy();
+
+            string actual = sut.ResolveTypeName(type);
+
+            actual.Should().Be(type.FullName);
+        }
+    }
+}
