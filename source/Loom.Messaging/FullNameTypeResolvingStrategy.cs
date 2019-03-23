5f0dc0cf00e15e0a5fd49e5ab791cc504ee09be8
Gyuwon <gyuwon@live.com>
2019-03-23 20:29:30 +0900

    Add class FullNameTypeResolvingStrategy
    FullNameTypeResolvingStrategy class provides an implementation of
    ITypeResolvingStrategy. It finds the type whose full name matches the
    specified type name.

---
+++ source/Loom.Messaging/FullNameTypeResolvingStrategy.cs
@@ -0,0 +1,12 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class FullNameTypeResolvingStrategy : ITypeResolvingStrategy
+    {
+        public Type TryResolveType(IEnumerable<Type> types, string typeName)
+            => types.SingleOrDefault(t => t.FullName == typeName);
+    }
+}
---
+++ source/Loom.Tests/Messaging/FullNameTypeResolvingStrategy_specs.cs
@@ -0,0 +1,46 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+    using AutoFixture;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class FullNameTypeResolvingStrategy_specs
+    {
+        [TestMethod]
+        public void sut_implements_ITypeResolvingStrategy()
+        {
+            Type sut = typeof(FullNameTypeResolvingStrategy);
+            sut.Should().Implement<ITypeResolvingStrategy>();
+        }
+
+        [TestMethod]
+        public void TryResolveType_returns_full_name_matching_type()
+        {
+            Type type = typeof(ReferencedType);
+            string fullName = type.FullName;
+            var sut = new FullNameTypeResolvingStrategy();
+            var resolver = new TypeResolver(
+                Mock.Of<ITypeNameResolvingStrategy>(), sut);
+
+            Type actual = resolver.TryResolveType(fullName);
+
+            actual.Should().Be(type);
+        }
+
+        [TestMethod]
+        public void given_unknown_type_name_then_TryResolveType_returns_null()
+        {
+            string fullName = new Fixture().Create<string>();
+            var sut = new FullNameTypeResolvingStrategy();
+            var resolver = new TypeResolver(
+                Mock.Of<ITypeNameResolvingStrategy>(), sut);
+
+            Type actual = resolver.TryResolveType(fullName);
+
+            actual.Should().BeNull();
+        }
+    }
+}
