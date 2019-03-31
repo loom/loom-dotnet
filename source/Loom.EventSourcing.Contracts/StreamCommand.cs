7a254f7136c6dccd31ae5eacfec47aa2ed3dd9a6
Gyuwon <gyuwon@live.com>
2019-03-31 20:30:07 +0900

    Add class StreamCommand<T>
    StreamCommand<T> encapsulates the identifier of the target stream and
    the command data.

---
+++ source/Loom.EventSourcing.Contracts/StreamCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+
+    public class StreamCommand<T>
+    {
+        public StreamCommand(Guid streamId, T data)
+            => (StreamId, Data) = (streamId, data);
+
+        public Guid StreamId { get; }
+
+        public T Data { get; }
+    }
+}
