2eea455b1139a6ff4803f1d7859733d7461341e5
Gyuwon <gyuwon@live.com>
2019-06-04 22:36:26 +0900

    Add class BlobSnapshotReader
    BlobSnapshotReader class provides an implementation of ISnapshotReader
    interface using Azure Blob storage.

---
+++ source/Loom.EventSourcing.Azure/BlobSnapshotReader.cs
@@ -0,0 +1,40 @@
+﻿namespace Loom.EventSourcing.Azure
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Storage.Blob;
+    using Newtonsoft.Json;
+
+    public class BlobSnapshotReader<T> : ISnapshotReader<T>
+    {
+        private readonly CloudBlobContainer _container;
+
+        public BlobSnapshotReader(CloudBlobContainer container)
+        {
+            _container = container;
+        }
+
+        public async Task<T> TryRestoreSnapshot(Guid streamId)
+        {
+            CloudBlockBlob blob = GetBlobReference(streamId);
+            return await blob.ExistsAsync()
+                 ? await RestoreSnapshot(blob)
+                 : default;
+        }
+
+        private CloudBlockBlob GetBlobReference(Guid streamId)
+        {
+            return _container.GetBlockBlobReference($"{streamId}.json");
+        }
+
+        private static async Task<T> RestoreSnapshot(CloudBlockBlob blob)
+        {
+            var stream = new MemoryStream();
+            await blob.DownloadToStreamAsync(stream);
+            string content = Encoding.UTF8.GetString(stream.ToArray());
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+    }
+}
---
+++ source/Loom.Tests/EventSourcing/Azure/BlobSnapshotReader_specs.cs
@@ -0,0 +1,74 @@
+﻿namespace Loom.EventSourcing.Azure
+{
+    using System;
+    using System.Threading.Tasks;
+    using AutoFixture;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using static StorageEmulator;
+
+    [TestClass]
+    public class BlobSnapshotReader_specs
+    {
+        public class State
+        {
+            public State(int value1, string value2, Guid value3)
+            {
+                Value1 = value1;
+                Value2 = value2;
+                Value3 = value3;
+            }
+
+            public int Value1 { get; }
+
+            public string Value2 { get; }
+
+            public Guid Value3 { get; }
+        }
+
+        [TestMethod]
+        public void sut_implements_ISnapshotReaderT()
+        {
+            typeof(BlobSnapshotReader<State>)
+                .Should().Implement<ISnapshotReader<State>>();
+        }
+
+        [TestMethod]
+        public async Task TryRestoreSnapshot_returns_null_if_snapshot_not_exists()
+        {
+            var sut = new BlobSnapshotReader<State>(SnapshotContainer);
+            var streamId = Guid.NewGuid();
+
+            State actual = await sut.TryRestoreSnapshot(streamId);
+
+            actual.Should().BeNull();
+        }
+
+        [TestMethod]
+        public async Task TryRestoreSnapshot_restores_snapshot_correctly_if_it_exists()
+        {
+            // Arrange
+            var sut = new BlobSnapshotReader<State>(SnapshotContainer);
+            var streamId = Guid.NewGuid();
+
+            State state = new Fixture().Create<State>();
+
+            IStateRehydrator<State> rehydrator =
+                Mock.Of<IStateRehydrator<State>>();
+            Mock.Get(rehydrator)
+                .Setup(x => x.TryRehydrateState(streamId))
+                .ReturnsAsync(state);
+
+            var snapshotter = new BlobSnapshotter<State>(
+                rehydrator, SnapshotContainer);
+            await snapshotter.TakeSnapshot(streamId);
+
+            // Act
+            State actual = await sut.TryRestoreSnapshot(streamId);
+
+            // Assert
+            actual.Should().BeEquivalentTo(state);
+        }
+    }
+}
