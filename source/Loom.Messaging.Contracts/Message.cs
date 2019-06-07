5a1cf4da1eb99877a5bd2a3a9643ff96fda2124b
Gyuwon <gyuwon@live.com>
2019-06-07 21:49:49 +0900

    Add class Message
    Loom.Messaging.Message class encapsulates message payload and metadata.

---
+++ source/Loom.Messaging.Contracts/Message.cs
@@ -0,0 +1,34 @@
+﻿namespace Loom.Messaging
+{
+    public sealed class Message
+    {
+        // TODO: Change the type of parameter 'operationId' to string?.
+        // TODO: Change the type of parameter 'contributor' to string?.
+        // TODO: Change the type of parameter 'parentId' to string?.
+        public Message(string id,
+                       string operationId,
+                       string contributor,
+                       string parentId,
+                       object data)
+        {
+            Id = id;
+            OperationId = operationId;
+            Contributor = contributor;
+            ParentId = parentId;
+            Data = data;
+        }
+
+        public string Id { get; }
+
+        // TODO: Change the type to string?.
+        public string OperationId { get; }
+
+        // TODO: Change the type to string?.
+        public string Contributor { get; }
+
+        // TODO: Change the type to string?.
+        public string ParentId { get; }
+
+        public object Data { get; }
+    }
+}
---
+++ source/Loom.Tests/Messaging/Message_specs.cs
@@ -0,0 +1,15 @@
+﻿namespace Loom.Messaging
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Message_specs
+    {
+        [TestMethod]
+        public void sut_is_sealed()
+        {
+            typeof(Message).Should().BeSealed();
+        }
+    }
+}
