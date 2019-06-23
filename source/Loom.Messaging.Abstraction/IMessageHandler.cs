3d55261808531ddbaaecc37225f55995266d5ba2
Gyuwon <gyuwon@live.com>
2019-06-23 20:25:53 +0900

    Add interface IMessageHandler

---
+++ source/Loom.Messaging.Abstraction/IMessageHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Loom.Messaging
+{
+    using System.Threading.Tasks;
+
+    public interface IMessageHandler
+    {
+        bool CanHandle(Message message);
+
+        Task Handle(Message message);
+    }
+}
