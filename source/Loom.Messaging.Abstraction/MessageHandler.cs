a3ffed48d3145e9d96898804bdeb941de2d7be17
Gyuwon <gyuwon@live.com>
2019-07-13 15:12:04 +0900

    Deprecate Loom.Messaging.MessageHandler<T>

--- source/Loom.Messaging.Abstraction/MessageHandler.cs
+++ source/Loom.Messaging.Abstraction/MessageHandler.cs
@@ -1,7 +1,9 @@
 ﻿namespace Loom.Messaging
 {
+    using System;
     using System.Threading.Tasks;
 
+    [Obsolete("Loom.Messaging.MessageHandler<T> class will be retired soon. Use your own implementations of Loom.Messaging.IMessageHandler instead.")]
     public abstract class MessageHandler<T> : IMessageHandler
     {
         public bool CanHandle(Message message) => message?.Data is T;
