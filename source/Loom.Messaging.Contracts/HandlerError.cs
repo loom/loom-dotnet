2fac803dacaf0dc2ef1894d2e974d98e6e83c1a1
Gyuwon <gyuwon@live.com>
2019-07-26 20:08:48 +0900

    Add class HandlerError
    HandlerError encapsulates information from error occured in a message
    handler.

---
+++ source/Loom.Messaging.Contracts/HandlerError.cs
@@ -0,0 +1,41 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class HandlerError
+    {
+        // TODO: Remove guard clauses after apply C# 8.0.
+        public HandlerError(string code,
+                            string message,
+                            string stackTrace,
+                            params HandlerError[] details)
+        {
+            if (details == null)
+            {
+                throw new ArgumentNullException(nameof(details));
+            }
+
+            foreach (HandlerError detail in details)
+            {
+                if (detail == null)
+                {
+                    throw new ArgumentException("Value should not contain null element.", nameof(details));
+                }
+            }
+
+            Code = code ?? throw new ArgumentNullException(nameof(code));
+            Message = message ?? throw new ArgumentNullException(nameof(message));
+            StackTrace = stackTrace ?? throw new ArgumentNullException(nameof(stackTrace));
+            Details = new List<HandlerError>(details).AsReadOnly();
+        }
+
+        public string Code { get; }
+
+        public string Message { get; }
+
+        public string StackTrace { get; }
+
+        public IEnumerable<HandlerError> Details { get; }
+    }
+}
