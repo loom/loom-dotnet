84d9c449d0ebd1f3de72d8e4fa99829cbc95ae55
Gyuwon <gyuwon@live.com>
2019-07-28 20:39:33 +0900

    Add class CompositePendingEventScanner

---
+++ source/Loom.EventSourcing.Abstraction/CompositePendingEventScanner.cs
@@ -0,0 +1,19 @@
+﻿namespace Loom.EventSourcing
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CompositePendingEventScanner : IPendingEventScanner
+    {
+        private readonly IEnumerable<IPendingEventScanner> _scanners;
+
+        public CompositePendingEventScanner(params IPendingEventScanner[] scanners)
+        {
+            _scanners = scanners.ToList().AsReadOnly();
+        }
+
+        public Task ScanPendingEvents()
+            => Task.WhenAll(_scanners.Select(s => s.ScanPendingEvents()));
+    }
+}
---
+++ source/Loom.Tests/EventSourcing/CompositePendingEventScanner_specs.cs
@@ -0,0 +1,46 @@
+﻿namespace Loom.EventSourcing
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Loom.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CompositePendingEventScanner_specs
+    {
+        [TestMethod]
+        public void sut_implements_IPendingEventScanner()
+        {
+            typeof(CompositePendingEventScanner).Should().Implement<IPendingEventScanner>();
+        }
+
+        public class Scanner : IPendingEventScanner
+        {
+            private readonly int _millisecondsDelay;
+
+            public Scanner([Range(1, 1000)] int millisecondsDelay)
+            {
+                _millisecondsDelay = millisecondsDelay;
+            }
+
+            public async Task ScanPendingEvents()
+            {
+                await Task.Delay(_millisecondsDelay);
+                Event.Set();
+            }
+
+            public ManualResetEvent Event { get; } = new ManualResetEvent(initialState: default);
+        }
+
+        [TestMethod, AutoData]
+        public async Task sut_invokes_all_scanners(Scanner[] scanners)
+        {
+            var sut = new CompositePendingEventScanner(scanners);
+            await sut.ScanPendingEvents();
+            scanners.All(s => s.Event.WaitOne(millisecondsTimeout: default)).Should().BeTrue();
+        }
+    }
+}
