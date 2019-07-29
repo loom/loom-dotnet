68831df2f2eb15ba234f696621b48edda6d746b5
Gyuwon <gyuwon@live.com>
2019-07-29 23:53:18 +0900

    Add project Loom.DataAnnotations
    Loom.DataAnnotations project provides ObjectValidator.

---
+++ source/Loom.DataAnnotations/ObjectValidator.cs
@@ -0,0 +1,311 @@
+﻿namespace Loom.DataAnnotations
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// Provides a helper class that can be used to validate objects. Unlike <see cref="Validator"/>, <see cref="ObjectValidator"/> traverses an object graph and uses full paths of properties as member names for <see cref="ValidationResult"/>.
+    /// </summary>
+    public static class ObjectValidator
+    {
+        private interface IVisitorStrategy
+        {
+            bool ShouldVisitProperty(object instance, PropertyInfo property);
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is valid. If not valid a <see cref="ValidationException"/> is thrown.
+        /// </summary>
+        /// <param name="instance">The object to validate.</param>
+        /// <exception cref="ValidationException">
+        /// The object is not valid.
+        /// </exception>
+        public static void Validate(object instance)
+        {
+            Visitor.Visit(
+                instance,
+                breakOnFirstError: true,
+                onError: error => throw error.ToException());
+        }
+
+        /// <summary>
+        /// Determine whether the specified object is valid. If the object is valid <paramref name="validationResult"/> is set to <see cref="ValidationResult.Success"/>; otherwise, a <see cref="ValidationResult"/> that contains error data.
+        /// </summary>
+        /// <param name="instance">The object to validate.</param>
+        /// <param name="validationResult">A <see cref="ValidationResult"/> object.</param>
+        /// <returns><c>true</c> if the object is valid; otherwise, <c>false</c>.</returns>
+        public static bool TryValidate(
+            object instance,
+            out ValidationResult validationResult)
+        {
+            ValidationResult captured = ValidationResult.Success;
+
+            Visitor.Visit(
+                instance,
+                breakOnFirstError: true,
+                onError: error => captured = error.ValidationResult);
+
+            validationResult = captured;
+            return validationResult == ValidationResult.Success;
+        }
+
+        /// <summary>
+        /// Determine whether the specified object is valid using a validation results collector.
+        /// </summary>
+        /// <param name="instance">The object to validate.</param>
+        /// <param name="validationResultCollector">A callback function called for each <see cref="ValidationResult"/> object indicating validation error.</param>
+        /// <returns><c>true</c> if the object is valid; otherwise, <c>false</c>.</returns>
+        public static bool TryValidate(
+            object instance,
+            Action<ValidationResult> validationResultCollector)
+        {
+            if (validationResultCollector == null)
+            {
+                throw new ArgumentNullException(nameof(validationResultCollector));
+            }
+
+            bool hasError = false;
+
+            Visitor.Visit(instance, breakOnFirstError: false, onError: error =>
+            {
+                hasError = true;
+                validationResultCollector.Invoke(error.ValidationResult);
+            });
+
+            return hasError == false;
+        }
+
+        private struct ValidationError
+        {
+            public ValidationError(
+                ValidationAttribute validationAttribute,
+                ValidationResult validationResult,
+                object value)
+            {
+                ValidationAttribute = validationAttribute;
+                ValidationResult = validationResult;
+                Value = value;
+            }
+
+            public ValidationAttribute ValidationAttribute { get; }
+
+            public ValidationResult ValidationResult { get; }
+
+            public object Value { get; }
+
+            public ValidationException ToException()
+                => new ValidationException(ValidationResult, ValidationAttribute, Value);
+        }
+
+        private class CompositeVisitorStrategy : IVisitorStrategy
+        {
+            private readonly IVisitorStrategy[] _strategies;
+
+            public CompositeVisitorStrategy(params IVisitorStrategy[] strategies)
+            {
+                _strategies = strategies;
+            }
+
+            public bool ShouldVisitProperty(object instance, PropertyInfo property)
+            {
+                foreach (IVisitorStrategy strategy in _strategies)
+                {
+                    if (strategy.ShouldVisitProperty(instance, property) == false)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        private class IndexerStrategy : IVisitorStrategy
+        {
+            public bool ShouldVisitProperty(object instance, PropertyInfo property)
+                => IsIndexer(property) == false;
+
+            private static bool IsIndexer(PropertyInfo property)
+                => property.GetIndexParameters().Any();
+        }
+
+        private class GetterStratrgy : IVisitorStrategy
+        {
+            public bool ShouldVisitProperty(object instance, PropertyInfo property)
+                => property.GetMethod?.IsStatic == false
+                && property.GetMethod?.IsPublic == true;
+        }
+
+        private class PrimitiveTypeInstanceStrategy : IVisitorStrategy
+        {
+            public bool ShouldVisitProperty(object instance, PropertyInfo property)
+                => instance.GetType().GetTypeInfo().IsPrimitive == false;
+        }
+
+        private class StringInstanceStratrgy : IVisitorStrategy
+        {
+            public bool ShouldVisitProperty(object instance, PropertyInfo property)
+                => (instance is string) == false;
+        }
+
+        private class DateTimeInstanceStrategy : IVisitorStrategy
+        {
+            public bool ShouldVisitProperty(object instance, PropertyInfo property)
+                => (instance is DateTime) == false;
+        }
+
+        private class DateTimeOffsetInstanceStrategy : IVisitorStrategy
+        {
+            public bool ShouldVisitProperty(object instance, PropertyInfo property)
+                => (instance is DateTimeOffset) == false;
+        }
+
+        private class Visitor
+        {
+            private readonly IVisitorStrategy _strategy =
+                new CompositeVisitorStrategy(
+                    new IndexerStrategy(),
+                    new GetterStratrgy(),
+                    new PrimitiveTypeInstanceStrategy(),
+                    new StringInstanceStratrgy(),
+                    new DateTimeInstanceStrategy(),
+                    new DateTimeOffsetInstanceStrategy());
+
+            private readonly Stack<object> _history = new Stack<object>();
+
+            private readonly bool _breakOnFirstError;
+            private readonly Action<ValidationError> _onError;
+            private bool _hasError;
+
+            private Visitor(bool breakOnFirstError, Action<ValidationError> onError)
+            {
+                _breakOnFirstError = breakOnFirstError;
+                _onError = onError;
+                _hasError = false;
+            }
+
+            private bool ShouldBreak => _hasError && _breakOnFirstError;
+
+            public static void Visit(
+                object instance,
+                bool breakOnFirstError,
+                Action<ValidationError> onError)
+            {
+                new Visitor(breakOnFirstError, onError).Visit(instance, prefix: string.Empty);
+            }
+
+            private void Visit(object instance, string prefix)
+            {
+                if (instance == null)
+                {
+                    return;
+                }
+
+                if (Visited(instance) == false)
+                {
+                    LeaveVisitRecord(instance);
+                    VisitProperties(instance, prefix);
+                    if (HasElements(instance, out IEnumerable enumerable))
+                    {
+                        VisitElements(enumerable, prefix);
+                    }
+                }
+            }
+
+            private bool Visited(object instance)
+                => _history.Any(x => ReferenceEquals(x, instance));
+
+            private void LeaveVisitRecord(object instance)
+                => _history.Push(instance);
+
+            private void VisitProperties(object instance, string prefix)
+            {
+                foreach (PropertyInfo property in GetVisiteeProperties(instance))
+                {
+                    string memberName = string.IsNullOrWhiteSpace(prefix)
+                        ? property.Name
+                        : $"{prefix}.{property.Name}";
+
+                    VisitProperty(instance, property, memberName);
+                }
+            }
+
+            private IEnumerable<PropertyInfo> GetVisiteeProperties(object instance)
+            {
+                return from type in AscendTypeHierarchy(instance)
+                       from property in GetDeclaredProperties(type)
+                       where ShouldVisit(instance, property)
+                       select property;
+            }
+
+            private IEnumerable<Type> AscendTypeHierarchy(object instance)
+            {
+                Type type = instance.GetType();
+                while (type != null)
+                {
+                    yield return type;
+                    type = type.GetTypeInfo().BaseType;
+                }
+            }
+
+            private IEnumerable<PropertyInfo> GetDeclaredProperties(Type type)
+                => type.GetTypeInfo().DeclaredProperties;
+
+            private bool ShouldVisit(object instance, PropertyInfo property)
+                => _strategy.ShouldVisitProperty(instance, property);
+
+            private void VisitProperty(
+                object instance, PropertyInfo property, string memberName)
+            {
+                object value = property.GetValue(instance);
+                foreach (ValidationAttribute validator in GetValidators(property))
+                {
+                    if (ShouldBreak)
+                    {
+                        return;
+                    }
+
+                    ValidateProperty(instance, value, memberName, validator);
+                }
+
+                Visit(value, prefix: memberName);
+            }
+
+            private static IEnumerable<ValidationAttribute> GetValidators(PropertyInfo property)
+                => property.GetCustomAttributes<ValidationAttribute>();
+
+            private void ValidateProperty(
+                object instance, object value, string memberName, ValidationAttribute validator)
+            {
+                var validationContext = new ValidationContext(instance) { MemberName = memberName };
+                ValidationResult result = validator.GetValidationResult(value, validationContext);
+                if (result != ValidationResult.Success)
+                {
+                    _hasError = true;
+                    _onError.Invoke(new ValidationError(validator, result, value));
+                }
+            }
+
+            private static bool HasElements(object instance, out IEnumerable enumerable)
+            {
+                enumerable = instance as IEnumerable;
+                return enumerable != null && (enumerable is string) == false;
+            }
+
+            private void VisitElements(IEnumerable enumerable, string prefix)
+            {
+                int index = 0;
+                foreach (object element in enumerable)
+                {
+                    string elementPrefix = $"{prefix}[{index}]";
+                    Visit(element, elementPrefix);
+                    index++;
+                }
+            }
+        }
+    }
+}
---
+++ source/Loom.Tests/DataAnnotations/ObjectValidator_specs.cs
@@ -0,0 +1,442 @@
+﻿namespace Loom.DataAnnotations
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class ObjectValidator_specs
+    {
+        public class RootObject
+        {
+            [Range(1, 10)]
+            public int Int32Property { get; set; } = 1;
+
+            public StemObject StemObjectProperty { get; set; } = new StemObject();
+
+            public IEnumerable CollectionProperty { get; set; } = Array.Empty<object>();
+        }
+
+        public class BaseObject
+        {
+            [Range(1, 10)]
+            public int BaseInt32Property { get; set; } = 1;
+        }
+
+        public class StemObject : BaseObject
+        {
+            [StringLength(10)]
+            public string StringProperty { get; set; } = "foo";
+        }
+
+        public class ElementObject
+        {
+            [Required]
+            public object ObjectProperty { get; set; } = new object();
+        }
+
+        public class WriteOnlyObject
+        {
+            public int WriteOnlyInt32Property
+            {
+                set
+                {
+                }
+            }
+        }
+
+        public interface ICollector
+        {
+            void Collect(ValidationResult validationResult);
+        }
+
+        [TestMethod]
+        public void collecting_TryValidate_has_guard_clause_against_null_validationResultCollector()
+        {
+            object instance = "foo";
+            Action action = () => ObjectValidator.TryValidate(instance, validationResultCollector: null);
+            action.Should().Throw<ArgumentNullException>().Which.ParamName.Should().Be("validationResultCollector");
+        }
+
+        [TestMethod]
+        public void given_null_argument_then_Validate_succeeds()
+        {
+            Action action = () => ObjectValidator.Validate(instance: null);
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        public void given_null_argument_then_TryValidate_succeeds()
+        {
+            object instance = null;
+
+            bool successful = ObjectValidator.TryValidate(instance, out ValidationResult validationResult);
+
+            successful.Should().BeTrue();
+            validationResult.Should().Be(ValidationResult.Success);
+        }
+
+        [TestMethod]
+        public void given_null_argument_then_collecting_TryValidate_succeeds()
+        {
+            object instance = null;
+            ICollector collector = Mock.Of<ICollector>();
+
+            bool successful = ObjectValidator.TryValidate(instance, collector.Collect);
+
+            successful.Should().BeTrue();
+            Mock.Get(collector)
+                .Verify(x => x.Collect(It.IsAny<ValidationResult>()), Times.Never());
+        }
+
+        [TestMethod]
+        public void given_valid_object_then_Validate_succeeds()
+        {
+            var instance = new RootObject();
+            Action action = () => ObjectValidator.Validate(instance);
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        public void given_valid_object_then_TryValidate_succeeds()
+        {
+            var instance = new RootObject();
+
+            bool successful = ObjectValidator.TryValidate(instance, out ValidationResult validationResult);
+
+            successful.Should().BeTrue();
+            validationResult.Should().Be(ValidationResult.Success);
+        }
+
+        [TestMethod]
+        public void given_valid_object_then_collecting_TryValidate_succeeds()
+        {
+            var instance = new RootObject();
+            ICollector collector = Mock.Of<ICollector>();
+
+            bool successful = ObjectValidator.TryValidate(instance, collector.Collect);
+
+            successful.Should().BeTrue();
+            Mock.Get(collector)
+                .Verify(x => x.Collect(It.IsAny<ValidationResult>()), Times.Never());
+        }
+
+        [TestMethod]
+        public void given_root_has_invalid_property_then_Validate_throws_ValidationException()
+        {
+            var instance = new RootObject
+            {
+                Int32Property = -1,
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().Throw<ValidationException>()
+                .Where(x => x.ValidationAttribute is RangeAttribute)
+                .Which.ValidationResult.MemberNames.Should()
+                .BeEquivalentTo("Int32Property");
+        }
+
+        [TestMethod]
+        public void given_root_has_invalid_property_then_TryValidate_fails()
+        {
+            var instance = new RootObject
+            {
+                Int32Property = -1,
+            };
+
+            bool successful = ObjectValidator.TryValidate(instance, out ValidationResult validationResult);
+
+            successful.Should().BeFalse();
+            validationResult.Should().NotBe(ValidationResult.Success);
+            validationResult.MemberNames.Should().BeEquivalentTo("Int32Property");
+        }
+
+        [TestMethod]
+        public void given_root_has_invalid_property_then_collecting_TryValidate_fails()
+        {
+            var instance = new RootObject
+            {
+                Int32Property = -1,
+            };
+            var validationResults = new List<ValidationResult>();
+
+            bool successful = ObjectValidator.TryValidate(instance, validationResults.Add);
+
+            successful.Should().BeFalse();
+            validationResults.Should().ContainSingle().Which.MemberNames.Should().BeEquivalentTo("Int32Property");
+        }
+
+        [TestMethod]
+        public void given_stem_has_invalid_property_then_Validate_throws_ValidationException()
+        {
+            var instance = new RootObject
+            {
+                StemObjectProperty =
+                {
+                    StringProperty = "f to the o to the o",
+                },
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().Throw<ValidationException>()
+                .Where(x => x.ValidationAttribute is StringLengthAttribute)
+                .Which.ValidationResult.MemberNames.Should()
+                .BeEquivalentTo("StemObjectProperty.StringProperty");
+        }
+
+        [TestMethod]
+        public void given_null_stem_then_Validate_succeeds()
+        {
+            var instance = new RootObject { StemObjectProperty = null };
+            Action action = () => ObjectValidator.Validate(instance);
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        public void given_invalid_collection_element_then_Validate_throws_ValidationException()
+        {
+            var instance = new RootObject
+            {
+                CollectionProperty = new[]
+                {
+                    new ElementObject(),
+                    new ElementObject { ObjectProperty = default },
+                },
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().Throw<ValidationException>()
+                .Where(x => x.ValidationAttribute is RequiredAttribute)
+                .Which.ValidationResult.MemberNames.Should()
+                .BeEquivalentTo("CollectionProperty[1].ObjectProperty");
+        }
+
+        [TestMethod]
+        public void given_null_element_then_Validate_succeeds()
+        {
+            var instance = new RootObject
+            {
+                CollectionProperty = new[]
+                {
+                    new ElementObject(),
+                    null,
+                },
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void given_circular_reference_then_Validate_succeeds()
+        {
+            var instance = new RootObject();
+            instance.CollectionProperty = new[] { instance };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        public void given_stem_has_invalid_inherited_property_then_Validate_throws_ValidationException()
+        {
+            var instance = new RootObject
+            {
+                StemObjectProperty =
+                {
+                    BaseInt32Property = -1,
+                },
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().Throw<ValidationException>()
+                .Where(x => x.ValidationAttribute is RangeAttribute)
+                .Which.ValidationResult.MemberNames.Should()
+                .BeEquivalentTo("StemObjectProperty.BaseInt32Property");
+        }
+
+        public static IEnumerable<object[]> FIXTURE_given_multiple_errors_then_TryValidate_returns_the_first_error
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new RootObject
+                    {
+                        Int32Property = -1,
+                        StemObjectProperty = { BaseInt32Property = -1 },
+                    },
+                    "Int32Property",
+                };
+
+                yield return new object[]
+                {
+                    new RootObject
+                    {
+                        Int32Property = 1,
+                        CollectionProperty = new[]
+                        {
+                            new ElementObject { ObjectProperty = null },
+                            new ElementObject { ObjectProperty = null },
+                        },
+                    },
+                    "CollectionProperty[0].ObjectProperty",
+                };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(FIXTURE_given_multiple_errors_then_TryValidate_returns_the_first_error))]
+        public void given_multiple_errors_then_TryValidate_returns_the_first_error(
+            object instance, string expectedMemberName)
+        {
+            // Act
+            bool successful = ObjectValidator.TryValidate(instance, out ValidationResult validationResult);
+
+            // Assert
+            successful.Should().BeFalse();
+            validationResult.Should().NotBe(ValidationResult.Success);
+            validationResult.MemberNames.Should().BeEquivalentTo(expectedMemberName);
+        }
+
+        public static IEnumerable<object[]> FIXTURE_given_multiple_errors_then_collecting_TryValidate_invokes_collector_function_for_all_errors
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new RootObject
+                    {
+                        Int32Property = -1,
+                        StemObjectProperty = { BaseInt32Property = -1 },
+                    },
+                    new[]
+                    {
+                        "Int32Property",
+                        "StemObjectProperty.BaseInt32Property",
+                    },
+                };
+
+                yield return new object[]
+                {
+                    new RootObject
+                    {
+                        Int32Property = 1,
+                        CollectionProperty = new[]
+                        {
+                            new ElementObject { ObjectProperty = null },
+                            new ElementObject { ObjectProperty = null },
+                        },
+                    },
+                    new[]
+                    {
+                        "CollectionProperty[0].ObjectProperty",
+                        "CollectionProperty[1].ObjectProperty",
+                    },
+                };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(FIXTURE_given_multiple_errors_then_collecting_TryValidate_invokes_collector_function_for_all_errors))]
+        public void given_multiple_errors_then_collecting_TryValidate_invokes_collector_function_for_all_errors(
+            object instance, string[] expectedMemberNames)
+        {
+            var validationResults = new List<ValidationResult>();
+
+            bool successful = ObjectValidator.TryValidate(instance, validationResults.Add);
+
+            successful.Should().BeFalse();
+            validationResults.Should().HaveSameCount(expectedMemberNames);
+            validationResults.SelectMany(r => r.MemberNames).Should().BeEquivalentTo(expectedMemberNames);
+        }
+
+        [TestMethod]
+        public void given_write_only_property_then_Validate_succeeds()
+        {
+            var instance = new RootObject
+            {
+                CollectionProperty = new[]
+                {
+                    new WriteOnlyObject(),
+                },
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        [Timeout(10)]
+        public void given_DateTime_element_then_Validate_does_not_enter_infinite_recursion()
+        {
+            var instance = new RootObject
+            {
+                CollectionProperty = new[]
+                {
+                    DateTime.Now,
+                },
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().NotThrow();
+        }
+
+        [TestMethod]
+        [Timeout(10)]
+        public void given_DateTimeOffset_element_then_Validate_does_not_enter_infinite_recursion()
+        {
+            var instance = new RootObject
+            {
+                CollectionProperty = new[]
+                {
+                    DateTimeOffset.Now,
+                },
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().NotThrow();
+        }
+
+        public interface IHasInt32Value
+        {
+            int Int32Value { get; }
+        }
+
+        public class HasNotSupportedExplicitProperty : IHasInt32Value
+        {
+            int IHasInt32Value.Int32Value => throw new NotSupportedException();
+        }
+
+        [TestMethod]
+        public void Validate_ignores_explicit_properties()
+        {
+            var instance = new RootObject
+            {
+                CollectionProperty = new[]
+                {
+                    new HasNotSupportedExplicitProperty(),
+                },
+            };
+
+            Action action = () => ObjectValidator.Validate(instance);
+
+            action.Should().NotThrow();
+        }
+    }
+}
