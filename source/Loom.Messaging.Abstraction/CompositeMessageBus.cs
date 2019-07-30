906bd0031c31cdc916c4155b2a32e5e51c6118b5
Gyuwon <gyuwon@live.com>
2019-07-30 23:02:58 +0900

    Add class CompositeMessageBus
    CompositeMessageBus implements IMessageBus interface using the composite
    design pattern.

---
+++ source/Loom.Messaging.Abstraction/CompositeMessageBus.cs
@@ -0,0 +1,18 @@
+﻿namespace Loom.Messaging
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CompositeMessageBus : IMessageBus
+    {
+        private readonly IEnumerable<IMessageBus> _buses;
+
+        public CompositeMessageBus(params IMessageBus[] buses)
+            => _buses = new ReadOnlyCollection<IMessageBus>(buses);
+
+        public Task Send(IEnumerable<Message> messages, string partitionKey)
+            => Task.WhenAll(_buses.Select(x => x.Send(messages, partitionKey)));
+    }
+}
---
+++ source/Loom.Tests/Messaging/CompositeMessageBus_specs.cs
@@ -0,0 +1,58 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Loom.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class CompositeMessageBus_specs
+    {
+        [TestMethod]
+        public void sut_implements_IMessageBus()
+        {
+            typeof(CompositeMessageBus).Should().Implement<IMessageBus>();
+        }
+
+        [TestMethod, AutoData]
+        public async Task Send_replays_to_all_buses(
+            IMessageBus[] buses, Message[] messages, string partitionKey)
+        {
+            var sut = new CompositeMessageBus(buses);
+            await sut.Send(messages, partitionKey);
+            buses.Select(Mock.Get).ForEach(bus => bus.Verify(x => x.Send(messages, partitionKey), Times.Once()));
+        }
+
+        [TestMethod, AutoData]
+        public async Task Send_replays_to_all_buses_even_if_some_fails(
+            IMessageBus[] buses, Message[] messages, string partitionKey)
+        {
+            // Arrange
+            Mock.Get(buses.Sample())
+                .Setup(x => x.Send(messages, partitionKey))
+                .ThrowsAsync(new InvalidOperationException());
+
+            var sut = new CompositeMessageBus(buses);
+
+            // Act
+            await TryForget(() => sut.Send(messages, partitionKey));
+
+            // Assert
+            buses.Select(Mock.Get).ForEach(bus => bus.Verify(x => x.Send(messages, partitionKey), Times.Once()));
+        }
+
+        private async Task TryForget(Func<Task> action)
+        {
+            try
+            {
+                await action.Invoke();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
