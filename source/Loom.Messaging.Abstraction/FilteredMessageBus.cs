c59f288d21e197a3328f99cfbaede0720ba9fa09
Gyuwon <gyuwon@live.com>
2019-07-30 22:21:48 +0900

    Add class FilteredMessageBus
    FilteredMessageBus implements IMessageBus interface. It sends only
    filterd messages.

---
+++ source/Loom.Messaging.Abstraction/FilteredMessageBus.cs
@@ -0,0 +1,24 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public sealed class FilteredMessageBus : IMessageBus
+    {
+        private readonly Func<Message, bool> _predicate;
+        private readonly IMessageBus _bus;
+
+        public FilteredMessageBus(Func<Message, bool> predicate, IMessageBus bus)
+        {
+            _predicate = predicate;
+            _bus = bus;
+        }
+
+        public Task Send(IEnumerable<Message> messages, string partitionKey)
+        {
+            return _bus.Send(messages.Where(_predicate).ToList().AsReadOnly(), partitionKey);
+        }
+    }
+}
---
+++ source/Loom.Tests/Messaging/FilteredMessageBus_specs.cs
@@ -0,0 +1,36 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Loom.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class FilteredMessageBus_specs
+    {
+        [TestMethod]
+        public void sut_implements_IMessageBus()
+        {
+            typeof(FilteredMessageBus).Should().Implement<IMessageBus>();
+        }
+
+        [TestMethod, AutoData]
+        public async Task sut_sends_only_filtered_messages(
+            IMessageBus bus, Message[] messages, string partitionKey)
+        {
+            Message[] sample = messages.Where(x => x.GetHashCode() % 2 == 0).ToArray();
+            Func<Message, bool> predicate = sample.Contains;
+            var sut = new FilteredMessageBus(predicate, bus);
+
+            await sut.Send(messages, partitionKey);
+
+            Expression<Func<IMessageBus, Task>> call = x => x.Send(It.Is<IEnumerable<Message>>(p => p.SequenceEqual(sample)), partitionKey);
+            Mock.Get(bus).Verify(expression: call, Times.Once());
+        }
+    }
+}
