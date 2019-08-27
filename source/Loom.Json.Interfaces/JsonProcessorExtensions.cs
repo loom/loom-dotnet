f874b21974ee855c9b9bd93f8818f8ebeb36c07f
Gyuwon <gyuwon@live.com>
2019-08-27 22:35:17 +0900

    Add FromJson<T> extension method

---
+++ source/Loom.Json.Interfaces/JsonProcessorExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Loom.Json
+{
+    using System;
+
+    public static class JsonProcessorExtensions
+    {
+        public static T FromJson<T>(this IJsonProcessor jsonProcessor, string json)
+        {
+            if (jsonProcessor is null)
+            {
+                throw new ArgumentNullException(nameof(jsonProcessor));
+            }
+
+            return (T)jsonProcessor.FromJson(json, dataType: typeof(T));
+        }
+    }
+}
