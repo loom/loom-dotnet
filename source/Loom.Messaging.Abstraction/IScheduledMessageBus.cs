c2737b373375bd1b1e020ace2b7d537079a85c30
Gyuwon <gyuwon@live.com>
2019-09-07 09:14:58 +0900

    Add interface IScheduledMessageBus
    The interface defines the functionality to send messages at the
    specified time. The time must be in UTC.

---
+++ source/Loom.Messaging.Abstraction/IScheduledMessageBus.cs
@@ -0,0 +1,13 @@
+﻿namespace Loom.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IScheduledMessageBus
+    {
+        Task Send(IEnumerable<Message> messages,
+                  string partitionKey,
+                  DateTime scheduledTimeUtc);
+    }
+}
