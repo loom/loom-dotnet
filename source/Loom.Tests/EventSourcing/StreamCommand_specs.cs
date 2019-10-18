57794cacea68622f2355e2f70527262f81d96078
Gyuwon <gyuwon@live.com>
2019-10-18 13:11:52 +0900

    Add StreamCommand.TryDecompose utility method

--- source/Loom.Tests/EventSourcing/StreamCommand_specs.cs
+++ source/Loom.Tests/EventSourcing/StreamCommand_specs.cs
@@ -15,12 +15,52 @@
         }
 
         [TestMethod, AutoData]
-        public void factory_creates_instance_correctly(Guid streamId, Command1 payload)
+        public void factory_creates_instance_correctly(
+            Guid streamId, Command1 payload)
         {
-            StreamCommand<Command1> actual = StreamCommand.Create(streamId, payload);
+            var actual = StreamCommand.Create(streamId, payload);
 
             actual.StreamId.Should().Be(streamId);
             actual.Payload.Should().Be(payload);
+        }
+
+        [TestMethod, AutoData]
+        public void TryDecompose_returns_true_if_value_is_stream_command(
+            StreamCommand<Command1> value)
+        {
+            bool actual = StreamCommand.TryDecompose(value, out _, out _);
+            actual.Should().BeTrue();
+        }
+
+        [TestMethod, AutoData]
+        public void TryDecompose_returns_false_if_value_is_non_stream_command(
+            StreamEvent<Event1> value)
+        {
+            bool actual = StreamCommand.TryDecompose(value, out _, out _);
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TryDecompose_returns_false_if_value_is_null()
+        {
+            bool actual = StreamCommand.TryDecompose(value: null, out _, out _);
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod, AutoData]
+        public void TryDecompose_returns_stream_id_correctly(
+            StreamCommand<Command1> value)
+        {
+            _ = StreamCommand.TryDecompose(value, out Guid streamId, out _);
+            streamId.Should().Be(value.StreamId);
+        }
+
+        [TestMethod, AutoData]
+        public void TryDecompose_returns_payload_correctly(
+            StreamCommand<Command1> value)
+        {
+            _ = StreamCommand.TryDecompose(value, out _, out object payload);
+            payload.Should().BeSameAs(value.Payload);
         }
     }
 }
