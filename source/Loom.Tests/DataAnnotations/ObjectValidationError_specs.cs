18cc494e5aa1eca37027a12fbade3018b74ee284
Gyuwon <gyuwon@lve.com>
2019-12-01 10:36:04 +0900

    Add a test case for ObjectValidationError

---
+++ source/Loom.Tests/DataAnnotations/ObjectValidationError_specs.cs
@@ -0,0 +1,43 @@
+﻿namespace Loom.DataAnnotations
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ObjectValidationError_specs
+    {
+        public class Component
+        {
+            [Range(1, 10)]
+            public int Value { get; set; } = 1;
+
+            public Component Child { get; set; }
+        }
+
+        [TestMethod]
+        public void MemberPaths_composes_path_correctly()
+        {
+            var instance = new Component
+            {
+                Child = new Component
+                {
+                    Child = new Component
+                    {
+                        Value = -1,
+                    },
+                },
+            };
+
+            ObjectValidator.TryValidate(
+                instance,
+                out IEnumerable<ObjectValidationError> errors);
+
+            ObjectValidationError sut = errors.Single();
+            sut.MemberPaths.Should().ContainSingle();
+            sut.MemberPaths.Single().Should().Be("Child.Child.Value");
+        }
+    }
+}
