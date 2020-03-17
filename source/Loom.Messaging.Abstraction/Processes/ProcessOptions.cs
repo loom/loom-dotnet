3a361e88de3edf4a0367a3f2779760090d3d401c
gyuwon <gyuwon@live.com>
2020-03-17 22:43:55 +0900

    Add ProcessOptions class
    work items: AB#25

---
+++ source/Loom.Messaging.Abstraction/Processes/ProcessOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace Loom.Messaging.Processes
+{
+    using System;
+
+    public sealed class ProcessOptions
+    {
+        public ProcessOptions(
+            Func<object, bool> completionDeterminer,
+            TimeSpan timeout)
+        {
+            CompletionDeterminer = completionDeterminer;
+            Timeout = timeout;
+        }
+
+        public Func<object, bool> CompletionDeterminer { get; }
+
+        public TimeSpan Timeout { get; }
+    }
+}
