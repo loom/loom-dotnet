0430ec7a64afdb3284d37bbd66fd4bf23053640d
suhyeon <flora12375@gmail.com>
2020-03-18 17:47:31 +0900

    Define IProcessEventCollector interface
    work items: AB#32

---
+++ source/Loom.Messaging.Abstraction/Processes/IProcessEventCollector.cs
@@ -0,0 +1,10 @@
+﻿namespace Loom.Messaging.Processes
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IProcessEventCollector
+    {
+        Task Collect(Message message, CancellationToken cancellationToken);
+    }
+}
