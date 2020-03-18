00391c6a10f1bb3e00beaf79f3af31f4ba44fa7c
suhyeon <flora12375@gmail.com>
2020-03-18 20:18:22 +0900

    Implement CosmosProcessEventCache
    Implement the CosmosProcessEventCache class that implements
    IProcessEventCollector interface and IProcessEventReader interface.
    
    work items: AB#35, AB#24

---
+++ source/Loom.Messaging.Azure/ProcessEventMessage.cs
@@ -0,0 +1,38 @@
+﻿namespace Loom.Messaging.Azure
+{
+    using Newtonsoft.Json;
+
+    internal class ProcessEventMessage
+    {
+        public ProcessEventMessage(
+            string id,
+            string dataType,
+            string dataJson,
+            string operationId,
+            string contributor,
+            string parentId)
+        {
+            Id = id;
+            DataType = dataType;
+            DataJson = dataJson;
+            OperationId = operationId;
+            Contributor = contributor;
+            ParentId = parentId;
+        }
+
+        public string Id { get; }
+
+        public string DataType { get; }
+
+        public string DataJson { get; }
+
+        public string OperationId { get; }
+
+        public string Contributor { get; }
+
+        public string ParentId { get; }
+
+        [JsonProperty("ttl")]
+        public int TimeToLiveInSeconds => 60 * 10;
+    }
+}
