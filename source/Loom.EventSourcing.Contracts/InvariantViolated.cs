75339f2c8c97eda38fdab18408a5a835da66f1f2
suhyeon <flora12375@gmail.com>
2020-04-02 17:46:21 +0900

    Define InvariantViolated event
    work items: AB#36

---
+++ source/Loom.EventSourcing.Contracts/InvariantViolated.cs
@@ -0,0 +1,21 @@
+﻿namespace Loom.EventSourcing
+{
+    using System;
+    using Loom.Messaging;
+
+    public sealed class InvariantViolated<T>
+    {
+        public InvariantViolated(Guid streamId, T command, ActivityError error)
+        {
+            StreamId = streamId;
+            Command = command;
+            Error = error;
+        }
+
+        public Guid StreamId { get; }
+
+        public T Command { get; }
+
+        public ActivityError Error { get; }
+    }
+}
