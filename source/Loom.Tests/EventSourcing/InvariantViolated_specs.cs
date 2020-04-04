df3cb548a10c3e869fc61223691344a2922d6883
suhyeon <flora12375@gmail.com>
2020-04-04 21:48:56 +0900

    Edit InvariantViolated event
    work items: AB#36

--- source/Loom.Tests/EventSourcing/InvariantViolated_specs.cs
+++ source/Loom.Tests/EventSourcing/InvariantViolated_specs.cs
@@ -10,15 +10,30 @@
     public class InvariantViolated_specs
     {
         [TestMethod, AutoData]
-        public void factory_method_creates_instance_correctly(
-            Guid streamId, Command1 command, ActivityError error)
+        public void factory_method_creates_instance_with_error_correctly(
+            StreamCommand<Command1> command, ActivityError error)
         {
-            var actual = InvariantViolated.Create(streamId, command, error);
+            var actual = InvariantViolated.Create(command, error);
 
             actual.Should().NotBeNull();
-            actual.StreamId.Should().Be(streamId);
             actual.Command.Should().BeEquivalentTo(command);
             actual.Error.Should().BeEquivalentTo(error);
+        }
+
+        [TestMethod, AutoData]
+        public void factory_method_creates_instance_with_exception_correctly(
+            StreamCommand<Command1> command, Exception exception)
+        {
+            var expectedError = new ActivityError(
+                exception.GetType().FullName,
+                exception.Message,
+                exception.StackTrace);
+
+            var actual = InvariantViolated.Create(command, exception);
+
+            actual.Should().NotBeNull();
+            actual.Command.Should().BeEquivalentTo(command);
+            actual.Error.Should().BeEquivalentTo(expectedError);
         }
     }
 }
