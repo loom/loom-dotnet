d6914add937e98e48fa98363f78ecb2d9e954368
gyuwon <gyuwon@live.com>
2020-04-15 18:51:02 +0900

    Add class ProcessCompletionStrategy<T>
    work items: AB#43

---
+++ source/Loom.Messaging.Abstraction/Processes/ProcessCompletionStrategy.cs
@@ -0,0 +1,25 @@
+﻿namespace Loom.Messaging.Processes
+{
+    using System;
+    using System.Collections.Generic;
+    using Loom.Messaging;
+
+    public sealed class ProcessCompletionStrategy<T>
+    {
+        public ProcessCompletionStrategy(
+            ProcessCompletionDeterminer<T> completionDeterminer,
+            TimeSpan timeout,
+            Func<IEnumerable<Message>, T> timeoutHandler)
+        {
+            CompletionDeterminer = completionDeterminer;
+            Timeout = timeout;
+            TimeoutHandler = timeoutHandler;
+        }
+
+        public ProcessCompletionDeterminer<T> CompletionDeterminer { get; }
+
+        public TimeSpan Timeout { get; }
+
+        public Func<IEnumerable<Message>, T> TimeoutHandler { get; }
+    }
+}
