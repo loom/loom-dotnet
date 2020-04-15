07efe90f74311a3f6ceeda28a99799867bb9bb4a
gyuwon <gyuwon@live.com>
2020-04-15 18:46:01 +0900

    Add delegate ProcessCompletionDeterminer<T>
    work items: AB#42

---
+++ source/Loom.Messaging.Abstraction/Processes/ProcessCompletionDeterminer.cs
@@ -0,0 +1,8 @@
+﻿namespace Loom.Messaging.Processes
+{
+    using System.Collections.Generic;
+
+    public delegate bool ProcessCompletionDeterminer<T>(
+        IEnumerable<Message> messages,
+        out T result);
+}
