d85e688dd45d62f30b395e27583659bc4437434d
suhyeon <flora12375@gmail.com>
2020-04-25 19:41:00 +0900

    Rename IProcessEventReader interface
    Rename IProcessEventReader interface to IProcessMessageReader.
    
    work items: AB#51

--- source/Loom.Messaging.Abstraction/Processes/IProcessMessageReader.cs
+++ source/Loom.Messaging.Abstraction/Processes/IProcessMessageReader.cs
@@ -4,7 +4,7 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    public interface IProcessEventReader
+    public interface IProcessMessageReader
     {
         Task<IEnumerable<Message>> Query(string operationId, CancellationToken cancellationToken);
     }
