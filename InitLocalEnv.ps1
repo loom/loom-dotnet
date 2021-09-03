9520d3b0b2b5dc12c2596f037c8e6c1b353ef549
Gyuwon Yi <gyuwon@live.com>
2021-09-04 02:12:55 +0900

    Fix InitLocalEnv.ps1
    Start Azurite container if it is installed but not running.

--- InitLocalEnv.ps1
+++ InitLocalEnv.ps1
@@ -4,17 +4,32 @@ function Write-Message {
   Write-Host "[$Timestamp] $Message"
 }
 
-Write-Message "Installing Azurite docker image..."
+function ContainerExists {
+  param([string]$Image, [string]$Name)
+  $Command = "docker ps --all --filter 'name=$Name'"
+  return (Invoke-Expression $Command).Where({ $_.Contains($Image) }).Count -gt 0
+}
+
+function IsContainerRunning {
+  param([string]$Image, [string]$Name)
+  $Command = "docker ps --filter 'name=$Name'"
+  return (Invoke-Expression $Command).Where({ $_.Contains($Image) }).Count -gt 0
+}
 
 $Image = "mcr.microsoft.com/azure-storage/azurite"
 $Name = "azurite"
 
+Write-Message "Installing Azurite docker image..."
 Invoke-Expression "docker pull $Image"
 
-if ((Invoke-Expression "docker ps --filter 'name=$Name'").Where({ $_.Contains($Image) }).Count -gt 0) {
-  Write-Message "Azurite in running"
+if (ContainerExists $Image $Name) {
+  if (-Not (IsContainerRunning $Image $Name)) {
+    Write-Message "Starting Azurite..."
+    Invoke-Expression "docker start $Name"
+  }
 }
 else {
   Write-Message "Running Azurite..."
   Invoke-Expression "docker run -d --name $Name -p 10000:10000 -p 10001:10001 -p 10002:10002 $Image"
 }
+Write-Message "Azurite in running"
