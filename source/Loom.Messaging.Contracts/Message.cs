3ef16755147c828b8eb3f2e464e81774359540ce
Gyuwon Yi <gyuwon@live.com>
2021-10-21 22:49:00 +0900

    Redesign metadata of class Message

--- source/Loom.Messaging.Contracts/Message.cs
+++ source/Loom.Messaging.Contracts/Message.cs
@@ -1,18 +1,33 @@
 ﻿namespace Loom.Messaging
 {
+    // TODO: Change to positional record.
     public sealed class Message
     {
+        // Change the signature to (string id, string processId, string initiator, string? predecessorId).
         public Message(string id, object data, TracingProperties tracingProperties)
         {
             Id = id;
+            ProcessId = tracingProperties.OperationId;
+            Initiator = tracingProperties.Contributor;
+            PredecessorId = tracingProperties.ParentId;
             Data = data;
-            TracingProperties = tracingProperties;
         }
 
         public string Id { get; }
 
+        public string ProcessId { get; }
+
+        // TODO: Change the type of the property to string from string?.
+        public string? Initiator { get; }
+
+        public string? PredecessorId { get; }
+
         public object Data { get; }
 
-        public TracingProperties TracingProperties { get; }
+        // TODO: Remove the property.
+        public TracingProperties TracingProperties => new TracingProperties(
+            operationId: ProcessId,
+            contributor: Initiator,
+            parentId: PredecessorId);
     }
 }
