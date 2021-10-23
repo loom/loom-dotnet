4d800266479e5a12fa8a832de90b6401c83d79cb
Gyuwon Yi <gyuwon@live.com>
2021-10-24 01:30:12 +0900

    Change class StreamCommand<T> to positional record

--- source/Loom.EventSourcing.Contracts/StreamCommand{T}.cs
+++ source/Loom.EventSourcing.Contracts/StreamCommand{T}.cs
@@ -1,12 +1,4 @@
 ﻿namespace Loom.EventSourcing
 {
-    public sealed class StreamCommand<T>
-    {
-        public StreamCommand(string streamId, T payload)
-            => (StreamId, Payload) = (streamId, payload);
-
-        public string StreamId { get; }
-
-        public T Payload { get; }
-    }
+    public sealed record StreamCommand<T>(string StreamId, T Payload);
 }
