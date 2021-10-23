3f893fb31b4598f19a9f8425c61938587d5a1503
Gyuwon Yi <gyuwon@live.com>
2021-10-24 01:28:37 +0900

    Change class StreamEvent<T> to positional record

--- source/Loom.EventSourcing.Contracts/StreamEvent{T}.cs
+++ source/Loom.EventSourcing.Contracts/StreamEvent{T}.cs
@@ -2,36 +2,9 @@
 
 namespace Loom.EventSourcing
 {
-    public sealed class StreamEvent<T>
-    {
-        public StreamEvent(string streamId,
-                           long version,
-                           DateTime raisedTimeUtc,
-                           T payload)
-        {
-            StreamId = streamId;
-            Version = version;
-            RaisedTimeUtc = raisedTimeUtc;
-            Payload = payload;
-        }
-
-        public string StreamId { get; }
-
-        public long Version { get; }
-
-        public DateTime RaisedTimeUtc { get; }
-
-        public T Payload { get; }
-
-        internal void Deconstruct(out string streamId,
-                                  out long version,
-                                  out DateTime raisedTimeUtc,
-                                  out T payload)
-        {
-            streamId = StreamId;
-            version = Version;
-            raisedTimeUtc = RaisedTimeUtc;
-            payload = Payload;
-        }
-    }
+    public sealed record StreamEvent<T>(
+        string StreamId,
+        long Version,
+        DateTime RaisedTimeUtc,
+        T Payload);
 }
