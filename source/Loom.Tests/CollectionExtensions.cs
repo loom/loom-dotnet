b036a1c10867702d1e2daf2ff8287d353fdaa277
Gyuwon Yi <gyuwon@live.com>
2021-10-24 04:01:57 +0900

    Update test dependencies

--- source/Loom.Tests/CollectionExtensions.cs
+++ source/Loom.Tests/CollectionExtensions.cs
@@ -1,12 +1,13 @@
-﻿namespace Loom
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace Loom
+{
     public static class CollectionExtensions
     {
-        private static readonly Random _random = new Random();
+        private static readonly Random _random = new();
 
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
@@ -23,6 +24,20 @@
         public static IEnumerable<T> Sample<T>(this IEnumerable<T> source, int count)
         {
             return source.Shuffle().Take(count);
+        }
+
+        public static bool TryGetValue(this IDictionary dictionary, object key, out object value)
+        {
+            if (dictionary.Contains(key))
+            {
+                value = dictionary[key];
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
         }
     }
 }
