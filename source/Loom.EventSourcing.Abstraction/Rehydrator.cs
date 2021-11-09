b9456c39ddae6b377f8358edc2166cbe06ec1aa7
Gyuwon Yi <gyuwon@live.com>
2021-11-10 00:01:04 +0900

    Add class Rehydrator<T>
    It restores state using past events.

---
+++ source/Loom.EventSourcing.Abstraction/Rehydrator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loom.EventSourcing
+{
+    public abstract class Rehydrator<T>
+    {
+        private readonly Func<string, T> _seedFactory;
+        private readonly IEventReader _eventReader;
+
+        protected Rehydrator(Func<string, T> seedFactory, IEventReader eventReader)
+        {
+            _seedFactory = seedFactory;
+            _eventReader = eventReader;
+        }
+
+        public async Task<Snapshot<T>> Rehydrate(
+            string streamId,
+            CancellationToken cancellationToken = default)
+        {
+            var events = new List<object>(await _eventReader.QueryEvents(streamId).ConfigureAwait(continueOnCapturedContext: false));
+            T seed = _seedFactory.Invoke(streamId);
+            T state = events.Aggregate(seed, HandleEvent);
+            return new(Version: events.Count, state);
+        }
+
+        private T HandleEvent(T state, object pastEvent)
+        {
+            MethodInfo handle = typeof(IEventHandler<,>)
+                .MakeGenericType(typeof(T), pastEvent.GetType())
+                .GetMethod("HandleEvent")!;
+
+            return (T)handle.Invoke(this, new[] { state, pastEvent })!;
+        }
+    }
+}
---
+++ source/Loom.Tests/EventSourcing/Rehydrator_specs.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Loom.EventSourcing.InMemory;
+using Loom.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loom.EventSourcing
+{
+    [TestClass]
+    public class Rehydrator_specs
+    {
+        [TestMethod]
+        public void sut_is_abstract()
+        {
+            typeof(Rehydrator<>).Should().BeAbstract();
+        }
+
+        public class Sut :
+            Rehydrator<State1>,
+            IEventHandler<State1, Event1>,
+            IEventHandler<State1, Event2>
+        {
+            private readonly Func<State1, Event1, State1> _handler1;
+            private readonly Func<State1, Event2, State1> _handler2;
+
+            public Sut(
+                Func<string, State1> seedFactory,
+                IEventReader eventReader,
+                Func<State1, Event1, State1> handler1 = null,
+                Func<State1, Event2, State1> handler2 = null)
+                : base(seedFactory, eventReader)
+            {
+                _handler1 = handler1;
+                _handler2 = handler2;
+            }
+
+            public State1 HandleEvent(State1 state, Event1 pastEvent)
+                => _handler1.Invoke(state, pastEvent);
+
+            public State1 HandleEvent(State1 state, Event2 pastEvent)
+                => _handler2.Invoke(state, pastEvent);
+        }
+
+        [TestMethod, AutoData]
+        public async Task sut_correctly_restores_state(
+            string streamId,
+            Event1 event1,
+            Event2 event2,
+            InMemoryEventStore<State1> eventStore)
+        {
+            // Arrange
+            object[] events = new object[] { event1, event2 };
+            await eventStore.CollectEvents(streamId, startVersion: 1, events);
+
+            Sut sut = new(
+                seedFactory: x => new(Hash(x)),
+                eventStore,
+                handler1: (s, e) => new State1(s.Value + e.Value),
+                handler2: (s, e) => new State1(s.Value + e.Value));
+
+            // Act
+            (int version, State1 state) = await sut.Rehydrate(streamId);
+
+            // Assert
+            int expected = Hash(streamId) + event1.Value + event2.Value;
+            state.Value.Should().Be(expected);
+        }
+
+        [TestMethod]
+        [InlineAutoData(2)]
+        [InlineAutoData(3)]
+        [InlineAutoData(4)]
+        public async Task sut_correctly_returns_version(
+            int count,
+            string streamId,
+            Generator<Event1> generator,
+            InMemoryEventStore<State1> eventStore)
+        {
+            // Arrange
+            Event1[] events = generator.Take(count).ToArray();
+            await eventStore.CollectEvents(streamId, startVersion: 1, events);
+
+            Sut sut = new(
+                seedFactory: x => new(Hash(x)),
+                eventStore,
+                handler1: (s, e) => new State1(s.Value + e.Value),
+                handler2: (s, e) => new State1(s.Value + e.Value));
+
+            // Act
+            (int version, State1 state) = await sut.Rehydrate(streamId);
+
+            // Assert
+            version.Should().Be(count);
+        }
+
+        private static int Hash(object x) => x.GetHashCode();
+    }
+}
