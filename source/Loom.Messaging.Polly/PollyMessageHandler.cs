a883674d03423c72de48c0f9522eea178519e13e
Gyuwon Yi <gyuwon@live.com>
2021-12-24 00:50:01 +0900

    Add class PollyMessageHandler
    PollyMessageHandler provides transient fault handling decorator for
    IMessageHandler.

---
+++ source/Loom.Messaging.Polly/PollyMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Polly;
+
+namespace Loom.Messaging
+{
+    public sealed class PollyMessageHandler : IMessageHandler
+    {
+        private readonly IAsyncPolicy _policy;
+        private readonly IMessageHandler _handler;
+
+        public PollyMessageHandler(IAsyncPolicy policy, IMessageHandler handler)
+            => (_policy, _handler) = (policy, handler);
+
+        public bool CanHandle(Message message)
+            => _handler.CanHandle(message);
+
+        public Task Handle(Message message, CancellationToken cancellationToken)
+            => _policy.ExecuteAsync(() => _handler.Handle(message, cancellationToken));
+    }
+}
---
+++ source/Loom.Tests/Messaging/PollyMessageHandler_specs.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Loom.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Polly;
+
+namespace Loom.Messaging
+{
+    [TestClass]
+    public class PollyMessageHandler_specs
+    {
+        [TestMethod]
+        [InlineAutoData(true)]
+        [InlineAutoData(false)]
+        public void sut_delegates_CanHandle(
+            bool answer,
+            IMessageHandler handler,
+            Message message,
+            IAsyncPolicy policy)
+        {
+            Mock.Get(handler).Setup(x => x.CanHandle(message)).Returns(answer);
+            PollyMessageHandler sut = new(policy, handler);
+
+            bool actual = sut.CanHandle(message);
+
+            actual.Should().Be(answer);
+        }
+
+        [TestMethod, AutoData]
+        public async Task sut_delegates_Handle(Message message)
+        {
+            MessageHandlerDouble spy = new();
+            IAsyncPolicy policy = Policy.Handle<Exception>().RetryAsync();
+            PollyMessageHandler sut = new(policy, handler: spy);
+
+            await sut.Handle(message, cancellationToken: default);
+
+            spy.Log.Should().ContainSingle().Which.Should().BeSameAs(message);
+        }
+
+        [TestMethod, AutoData]
+        public async Task sut_retries_if_handler_fails(
+            Exception[] exceptions,
+            Message message)
+        {
+            MessageHandlerDouble spy = new(exceptions);
+            IAsyncPolicy policy = Policy
+                .Handle<Exception>()
+                .RetryAsync(retryCount: exceptions.Length);
+            PollyMessageHandler sut = new(policy, handler: spy);
+
+            await sut.Handle(message, cancellationToken: default);
+
+            spy.Log.Should().ContainSingle().Which.Should().BeSameAs(message);
+        }
+
+        public class MessageHandlerDouble : IMessageHandler
+        {
+            private readonly Queue<Exception> _exceptions;
+            private readonly Queue<Message> _log = new();
+
+            public MessageHandlerDouble(params Exception[] exceptions)
+            {
+                _exceptions = new(exceptions);
+            }
+
+            public bool CanHandle(Message message) => true;
+
+            public IEnumerable<Message> Log => _log;
+
+            public async Task Handle(Message message, CancellationToken cancellationToken)
+            {
+                await Task.Delay(1, cancellationToken);
+
+                if (_exceptions.TryDequeue(out Exception exception))
+                {
+                    throw exception;
+                }
+
+                _log.Enqueue(message);
+            }
+        }
+    }
+}
