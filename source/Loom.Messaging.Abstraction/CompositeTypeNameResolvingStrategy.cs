b7f834057d0962931c95e24a92690a3556467274
Vader Yi <vader.yi@healingpaper.com>
2022-01-27 22:48:22 +0900

    Add class CompositeTypeNameResolvingStrategy
    It provides the composite pattern for ITypeNameResolvingStrategy
    interface.

---
+++ source/Loom.Messaging.Abstraction/CompositeTypeNameResolvingStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Loom.Messaging
+{
+    public sealed class CompositeTypeNameResolvingStrategy :
+        ITypeNameResolvingStrategy
+    {
+        private readonly ImmutableArray<ITypeNameResolvingStrategy> _strategies;
+
+        public CompositeTypeNameResolvingStrategy(
+            params ITypeNameResolvingStrategy[] strategies)
+        {
+            _strategies = ImmutableArray.CreateRange(strategies);
+        }
+
+        public string? TryResolveTypeName(Type type)
+        {
+            return _strategies
+                .Select(strategy => strategy.TryResolveTypeName(type))
+                .FirstOrDefault(result => result != null);
+        }
+    }
+}
---
+++ source/Loom.Tests/Messaging/CompositeTypeNameResolvingStrategy_specs.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using FluentAssertions;
+using Loom.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loom.Messaging;
+
+[TestClass]
+public class CompositeTypeNameResolvingStrategy_specs
+{
+    [TestMethod]
+    public void sut_implements_ITypeNameResolvingStrategy()
+    {
+        Type tut = typeof(CompositeTypeNameResolvingStrategy);
+        tut.Should().Implement<ITypeNameResolvingStrategy>();
+    }
+
+    [TestMethod, AutoData]
+    public void sut_returns_first_result(
+        [Frozen] Type type,
+        StrategyStub[] stubs)
+    {
+        CompositeTypeNameResolvingStrategy sut = new(strategies: stubs);
+        string? actual = sut.TryResolveTypeName(type);
+        actual.Should().Be(stubs[0].Name);
+    }
+
+    [TestMethod, AutoData]
+    public void sut_skips_null(Type type, string name)
+    {
+        CompositeTypeNameResolvingStrategy sut = new(
+            new StrategyStub(type, null),
+            new StrategyStub(type, null),
+            new StrategyStub(type, name));
+
+        string? actual = sut.TryResolveTypeName(type);
+
+        actual.Should().Be(name);
+    }
+
+    [TestMethod, AutoData]
+    public void sut_returns_null_if_all_strategies_return_null(Type type)
+    {
+        CompositeTypeNameResolvingStrategy sut = new(
+            new StrategyStub(type, null),
+            new StrategyStub(type, null),
+            new StrategyStub(type, null));
+
+        string? actual = sut.TryResolveTypeName(type);
+
+        actual.Should().BeNull();
+    }
+
+    public sealed class StrategyStub : ITypeNameResolvingStrategy
+    {
+        public StrategyStub(Type type, string? name)
+        {
+            Type = type;
+            Name = name;
+        }
+
+        public Type Type { get; }
+
+        public string? Name { get; }
+
+        public string? TryResolveTypeName(Type type)
+            => type == Type ? Name : null;
+    }
+}
