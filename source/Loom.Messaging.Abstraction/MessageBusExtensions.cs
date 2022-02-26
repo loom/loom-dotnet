90fec0c6f14ecce7f57855e2e8bd1708a6e8463b
Gyuwon Yi <gyuwon@live.com>
2022-02-26 20:18:39 +0900

    Add extension method to IMessageBus interface

---
+++ source/Loom.Messaging.Abstraction/MessageBusExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loom.Messaging
+{
+    public static class MessageBusExtensions
+    {
+        public static Task Send(
+            this IMessageBus bus,
+            Message message,
+            string partitionKey,
+            CancellationToken cancellationToken = default)
+        {
+            if (bus is null)
+            {
+                throw new ArgumentNullException(nameof(bus));
+            }
+
+            return bus.Send(new[] { message }, partitionKey, cancellationToken);
+        }
+    }
+}
---
+++ source/Loom.Tests/Messaging/MessageBusExtensions_specs.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Loom.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loom.Messaging;
+
+[TestClass]
+public class MessageBusExtensions_specs
+{
+    [TestMethod, AutoData]
+    public async Task Send_calls_instance_method(
+        MessageBusDouble spy,
+        Message message,
+        string partitionKey)
+    {
+        await spy.Send(message, partitionKey);
+        spy.Calls.Should().ContainSingle();
+    }
+
+    [TestMethod, AutoData]
+    public async Task Send_correctly_sends_single_message(
+        MessageBusDouble spy,
+        Message message,
+        string partitionKey)
+    {
+        await spy.Send(message, partitionKey);
+
+        ImmutableArray<Message> messages = spy.Calls.Single().Messages;
+        messages.Should().ContainSingle().Which.Should().Be(message);
+    }
+
+    [TestMethod, AutoData]
+    public async Task Send_sends_single_message_with_correct_partition_key(
+        MessageBusDouble spy,
+        Message message,
+        string partitionKey)
+    {
+        await spy.Send(message, partitionKey);
+        spy.Calls.Single().PartitionKey.Should().Be(partitionKey);
+    }
+}
